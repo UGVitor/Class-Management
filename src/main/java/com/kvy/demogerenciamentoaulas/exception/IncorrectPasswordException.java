d4d9f0902ecfbf800d77d40109b74f07a61c9653
Vitor Dias <110608624+UGVitor@users.noreply.github.com>
2024-10-21 19:40:06 -0300

    Delete src directory
    Não tinha utilidade

--- src/main/java/com/kvy/demogerenciamentoaulas/exception/IncorrectPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.kvy.demogerenciamentoaulas.exception;
-
-public class IncorrectPasswordException extends RuntimeException {
-    public IncorrectPasswordException(String message) {
-        super(message);
-    }
-}
