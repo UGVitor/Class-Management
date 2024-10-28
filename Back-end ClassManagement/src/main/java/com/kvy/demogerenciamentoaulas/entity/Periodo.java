c92cf1d28785b8b49dd1f180596a5890d881a95b
Yanka Ayala <yankaayala26@gmail.com>
2024-10-28 11:37:17 -0300

    Criação do Crud de Periodo, Periodo expection, periodo repository, periodo service e periodo controller.  E algumas modoficações no front.

--- Back-end ClassManagement/src/main/java/com/kvy/demogerenciamentoaulas/entity/Periodo.java
+++ Back-end ClassManagement/src/main/java/com/kvy/demogerenciamentoaulas/entity/Periodo.java
@@ -1,19 +1,49 @@
 package com.kvy.demogerenciamentoaulas.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
+
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Data
 @Entity
 @Table(name = "periodo")
 public class Periodo implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        private Long id;
 
-    @Column(name = "periodo", nullable = false)
-    private String periodo;
+        @Column(name = "nome", nullable = false)
+        private String nome;
+
+        @OneToMany(mappedBy = "periodo", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+        @JsonManagedReference("periodo-turma")
+        private Set<Turma> turmas = new HashSet<>();
+
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Periodo that = (Periodo) o;
+                return Objects.equals(id, that.id);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hashCode(id);
+        }
+
+        @Override
+        public String toString() {
+                return "Periodo{" +
+                        "id=" + id +
+                        '}';
+        }
 }
---
+++ Back-end ClassManagement/src/main/java/com/kvy/demogerenciamentoaulas/web/controller/PeriodoController.java
@@ -0,0 +1,81 @@
+package com.kvy.demogerenciamentoaulas.web.controller;
+
+import com.kvy.demogerenciamentoaulas.entity.Periodo;
+import com.kvy.demogerenciamentoaulas.service.PeriodoService;
+import com.kvy.demogerenciamentoaulas.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "Periodos", description = "Contém todas as operações relativas aos recursos de CRUD de periodo.")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/periodos")
+
+public class PeriodoController {
+
+    private final PeriodoService periodoService;
+
+    @Operation(summary = "Criar um novo periodo", description = "Recurso para criar um novo periodo",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Periodo.class))),
+                    @ApiResponse(responseCode = "409", description = "periodo já cadastrado no sistema",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recursos não processado por dados de entrada invalidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
+    @PostMapping
+    public ResponseEntity<Periodo> createPeriodo(@RequestBody Periodo periodo) {
+        Periodo savedPeriodo = periodoService.salvar(periodo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPeriodo);
+    }
+
+    @Operation(summary = "Recuperar um periodo pelo id", description = "Recuperar um periodo pelo id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Periodo.class))),
+                    @ApiResponse(responseCode = "404", description = "Recursos não encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Periodo> getPeriodoById(@PathVariable Long id) {
+        Periodo periodo = periodoService.buscarPorId(id);
+        return ResponseEntity.ok(periodo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Periodo> updatePeriodo(@PathVariable Long id, @RequestBody Periodo periodo) {
+        Periodo updatedPeriodo = periodoService.editar(id, periodo);
+        return ResponseEntity.ok(updatedPeriodo);
+    }
+
+    @Operation(summary = "Excluir periodo", description = "Recurso para excluir um periodo pelo ID",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Periodo excluído com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class))),
+                    @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePeriodo(@PathVariable Long id) {
+        periodoService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Periodo>> getPeriodoAll() {
+        List<Periodo> periodos = periodoService.buscarTodos();
+        return ResponseEntity.ok(periodos);
+    }
+}
