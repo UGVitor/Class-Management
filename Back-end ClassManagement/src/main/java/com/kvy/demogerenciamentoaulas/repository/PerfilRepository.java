8b730e1c5e9062f3a93df4121e0a14408dbaa7c0
kauagabr <kaua32475@gmail.com>
2025-01-18 20:02:59 -0300

    Ajuste no back-end.

--- Back-end ClassManagement/src/main/java/com/kvy/demogerenciamentoaulas/repository/PerfilRepository.java
+++ Back-end ClassManagement/src/main/java/com/kvy/demogerenciamentoaulas/repository/PerfilRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 @Repository
 public interface PerfilRepository extends JpaRepository<Perfil, Long> {
     Optional<Perfil> findByNome(String nome);
+
+    boolean existsByNome(String nome);
 }
